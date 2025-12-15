23b0607861de047a00b3a77c2f31c338a5493231
true_mogician <truemogician@gmail.com>
2025-12-14 22:31:18 -0800

    fix: settings ui stat display

--- Setosa's Power Tools/Source/Mod.cs
+++ Setosa's Power Tools/Source/Mod.cs
@@ -10,24 +10,25 @@ namespace Setosa;
 
 [StaticConstructorOnStartup]
 public class Mod : Verse.Mod {
-	private readonly ScrollView _scroll = new();
-
-	public Mod(ModContentPack content) : base(content) {
-		Settings.Default = GetSettings<Settings>();
-    }
-
 	private const float RowHeight = 30;
 
-	private static string FormatValue(float value) {
-		var color = value switch {
+	private readonly ScrollView _scroll = new();
+
+	public Mod(ModContentPack content) : base(content) => Settings.Default = GetSettings<Settings>();
+
+	private static string FormatValue(float value, int digits = 2, bool percent = true) {
+		string color = value switch {
 			> 0f => "green",
 			0    => "white",
 			_    => "red"
 		};
-		return $"<color={color}>{value:+0.##%;-0.##%;0%}</color>";
+		string digitFormat = string.Concat(Enumerable.Repeat("#", digits));
+		string[] formats = [$"+0.{digitFormat}", $"-0.{digitFormat}"];
+		string? format = string.Join(';', percent ? formats.Select(f => f + '%') : formats);
+		return $"<color={color}>{value.ToString(format)}</color>";
 	}
 
-    public override string SettingsCategory() => ThisAssembly.Info.Title;
+	public override string SettingsCategory() => ThisAssembly.Info.Title;
 
 	public override void DoSettingsWindowContents(Rect inRect) {
 		var list = new Listing_Standard();
@@ -52,27 +53,25 @@ public class Mod : Verse.Mod {
 		float height = (groups.Count + (Settings.Default.Preset == StatsPreset.Custom ? Settings.Default.Offsets.Count : groups.Count)) * RowHeight;
 
 		var rest = new Rect(inRect) { y = list.CurHeight, height = inRect.height - list.CurHeight };
-        var scrollList = _scroll.Begin(rest, height);
+		var scrollList = _scroll.Begin(rest, height);
 
 		var customUpdates = new List<ThingStatOffset>();
-		foreach (var (thingDef, offsets) in groups) {
+		foreach ((string? thingDef, var offsets) in groups) {
 			var thing = DefDatabase<ThingDef>.GetNamed(thingDef, false);
 			if (thing is null) {
 				Logger.Warning($"Thing {thingDef} not found", true);
 				continue;
 			}
-			using (new TransientStyle { Anchor = TextAnchor.MiddleCenter })
-				scrollList.Label($"<b>{thing.LabelCap}</b>");
+			scrollList.Label($"<b>{thing.LabelCap}</b>");
 			if (Settings.Default.Preset != StatsPreset.Custom) {
 				var texts = new List<string>();
 				foreach (var tuple in offsets) {
 					if (tuple.Stat is { } stat)
-						texts.Add($"{FormatValue(tuple.Value)} {stat.label}");
+						texts.Add($"{FormatValue(tuple.Value, percent: stat.defName != "MoveSpeed")} {stat.label}");
 					else
 						Logger.Warning($"Stat {tuple.StatDef} not found", true);
 				}
-				using (new TransientStyle { Anchor = TextAnchor.MiddleCenter })
-                    Widgets.Label(scrollList.GetRect(RowHeight), string.Join(", ", texts));
+				Widgets.Label(scrollList.GetRect(RowHeight), string.Join(", ", texts));
 			}
 			else {
 				foreach (var tuple in offsets) {
@@ -83,14 +82,14 @@ public class Mod : Verse.Mod {
 					}
 					var cols = scrollList.GetRect(RowHeight).FlexBox(["250", "50", "1fr"]);
 					Widgets.Label(cols[0], $"{stat.LabelCap}:");
-					Widgets.Label(cols[1], FormatValue(tuple.Value));
+					Widgets.Label(cols[1], FormatValue(tuple.Value, percent: stat.defName != "MoveSpeed"));
 					if (Settings.Default.Preset == StatsPreset.Custom) {
 						float maximum = Presets.Normal[tuple.ThingDef, tuple.StatDef] * 4;
 						float newValue = Widgets.HorizontalSlider(
-							rect: cols[2],
-							value: tuple.Value,
-							min: Math.Min(0f, maximum),
-							max: Math.Max(0f, maximum),
+							rect: cols[2], 
+							value: tuple.Value, 
+							min: Math.Min(0f, maximum), 
+							max: Math.Max(0f, maximum), 
 							roundTo: 0.1f
 						);
 						if (!Mathf.Approximately(tuple.Value, newValue))
@@ -103,7 +102,7 @@ public class Mod : Verse.Mod {
 		_scroll.End();
 		list.End();
 
-        if (customUpdates.Count > 0) {
+		if (customUpdates.Count > 0) {
 			var newCustomOffsets = Settings.Default.CustomOffsets.Clone();
 			foreach ((string thing, string stat, float value) in customUpdates)
 				newCustomOffsets[thing, stat] = value;
